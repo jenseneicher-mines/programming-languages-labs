25636f158e03c1fee45dd6c7d7abdf09130c7487
jensen <jeicher@mymail.mines.edu>
2020-02-17 11:28:57 -0700

    Added most failing tests for ListDemoTest.scala

--- src/test/scala/ListDemoTest.scala
+++ src/test/scala/ListDemoTest.scala
@@ -8,14 +8,27 @@ class ListDemoTest extends FlatSpec {
   "Selection Sort" should "return a sorted list of five integers" in {
     assert(ld.selectionSort(List(3,2,4,1,5)) === List(1,2,3,4,5))
   }
+  "Selection Sort" should "return a sorted list of four integers" in {
+    assert(ld.selectionSort(List(3,2,4,1)) === List(1,2,3,4))
+  }
+  "Selection Sort" should "return a sorted list of six integers" in {
+    assert(ld.selectionSort(List(12,2,6,10,7,9)) === List(2,6,7,9,10,12))
+  }
+  "Selection Sort" should "return a sorted list of ten integers" in {
+    assert(ld.selectionSort(List(9,3,6,4,1,10,27,14,12,2)) === List(1,2,3,4,6,9,10,12,14,27))
+  }
+  "Selection Sort" should "return a sorted list of sorted integers" in {
+    assert(ld.selectionSort(List(1,2,3,4,5,6)) === List(1,2,3,4,5,6))
+  }
+  "Selection Sort" should "return a sorted list of reverse sorted integers" in {
+    assert(ld.selectionSort(List(7,6,5,4,3,2,1)) === List(1,2,3,4,5,6,7))
+  }
 
-  // MAP +1
+
+  // MAP
   "Map" should "work properly with a basic +1 function" in {
     assert(ld.map(List(1,2,3,4,5),(x:Int)=>x+1) === List(2,3,4,5,6))
   }
-
-  // MAP integer string
-
   it should "work properly with a basic integer-to-string conversion" in {
     assert(ld.map(List(1,2,3,4,5),(x:Int)=>"$"+x) === List("$1","$2","$3","$4","$5"))
   }
@@ -47,13 +60,45 @@ class ListDemoTest extends FlatSpec {
     assert(ld.foldRight(List(1,2,3),123,(y:Int,x:Int)=>y+x) === 6 + 123)
   }
 
+
   // FILTER
   "Filter" should "properly capture integers greater than a bound" in {
     assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x > 3)) === List(4,5,6))
   }
+  "Filter" should "properly capture integers less than a bound" in {
+    assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x < 3)) === List(1,2))
+  }
+  "Filter" should "properly capture integers greater than or equal to a bound" in {
+    assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x >= 3)) === List(3,4,5,6))
+  }
+  "Filter" should "properly capture integers not equal to a bound" in {
+    assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x != 6)) === List(1,2,3,4,5))
+  }
+  "Filter" should "properly capture integers less than or equal to a bound" in {
+    assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x <= 3)) === List(1,2,3))
+  }
+  "Filter" should "properly capture integers equal to a bound" in {
+    assert(ld.filter(List(1,4,2,5,3,6),(x:Int)=>(x == 3)) === List(3))
+  }
+
 
   // REVERSE
   "Reverse" should "properly reverse a list of integers" in {
     assert(ld.reverse(List(1,2,3,4)) === List(4,3,2,1))
+  }
+  "Reverse" should "properly reverse a list of integers #2" in {
+    assert(ld.reverse(List(1,2,3,4,5,6,7)) === List(7,6,5,4,3,2,1))
+  }
+  "Reverse" should "properly reverse a list of integers #3" in {
+    assert(ld.reverse(List(2,7,4,9,1)) === List(1,9,4,7,2))
+  }
+  "Reverse" should "properly reverse a list of integers #4" in {
+    assert(ld.reverse(List(2,10,6,3,2,1)) === List(1,2,3,6,10,2))
+  }
+  "Reverse" should "properly reverse a list of integers #5" in {
+    assert(ld.reverse(List(1,1,1,1,1,1,2,3,4,1,1,2)) === List(2,1,1,4,3,2,1,1,1,1,1,1))
+  }
+  "Reverse" should "properly reverse a list of integers #6" in {
+    assert(ld.reverse(List(1)) === List(1))
   }
 }
