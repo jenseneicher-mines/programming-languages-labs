cad37912bf2917e993a305cdd54a69865aa39927
jensen <jeicher@mymail.mines.edu>
2020-02-19 18:50:11 -0700

    Added failing searchBST tests

--- src/test/scala/TreeDemoTest.scala
+++ src/test/scala/TreeDemoTest.scala
@@ -201,14 +201,42 @@ class TreeDemoTest extends FlatSpec {
         assert(td.isBST(Leaf(4)) === true)
     }
 
+
     "BST Insert" should "give the correct answer for integer example" in {
         assert(td.insertBST(myTree,6) === td.replace(myTree,5,Node(Empty,5,Leaf(6))))
     } 
 
-    "BST Search" should "give the correct answer for non-existant element" in {
+    "BST Search" should "give the correct output for a string tree" in {
+        val td2 = new TreeDemo[String]
+        import td2._
+
+        val myStringTree = Node(Node(Leaf("b"),"g",Node(Leaf("h"),"i",Leaf("j"))),"k",Leaf("l"))
+
+         /*
+          this encodes the following tree:
+                  k
+               /     \
+              g       l
+             / \    
+            b   i
+               / \ 
+              h   j
+          */
+        assert(td2.searchBST(myStringTree,"a") === false)
+        assert(td2.searchBST(myStringTree,"j") === true)
+        assert(td2.searchBST(myStringTree,"g") === true)
+        assert(td2.searchBST(myStringTree,"k") === true)
+    }
+    it should "give the correct answer for non-existant element" in {
         assert(td.searchBST(myTree,6) === false)
     }
-    it should "give the correct answer for integer example" in {
+    it should "give the correct answer for integer example Leaf" in {
+        assert(td.searchBST(myTree,1) === true)
+    }
+    it should "give the correct answer for integer example Middle" in {
         assert(td.searchBST(myTree,2) === true)
+    }
+    it should "give the correct answer for integer example Root" in {
+        assert(td.searchBST(myTree,4) === true)
     }
 }
