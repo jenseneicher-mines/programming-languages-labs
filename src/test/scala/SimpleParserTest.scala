2166318af01598b41d6b04d29043e072fc2375d9
jensen <jeicher@mymail.mines.edu>
2020-04-01 18:30:42 -0600

    Added 5 new tests for simple parser

--- src/test/scala/SimpleParserTest.scala
+++ src/test/scala/SimpleParserTest.scala
@@ -15,4 +15,19 @@ class SimpleParserTest extends FlatSpec {
   it should "properly compute difference" in {
     assert(SimpleParser.parse("1.0*2.0-3.0") === BopExpr(BopExpr(ve(1.0f), TimesBop, ve(2.0f)), MinusBop, ve(3.0f)))
   }
+  it should "properly compute division" in {
+    assert(SimpleParser.parse("4.0/2.0") === BopExpr(ve(4.0f), DivBop, ve(2.0f)))
+  }
+  it should "properly compute AND" in {
+    assert(SimpleParser.parse("true&&false") === BopExpr(ve(true), AndBop, ve(false)))
+  }
+  it should "properly compute OR" in {
+    assert(SimpleParser.parse("true||false") === BopExpr(ve(true), OrBop, ve(false)))
+  }
+  it should "properly compute multiplicaiton" in {
+    assert(SimpleParser.parse("4.0*2") === BopExpr(ve(4.0f), TimesBop, ve(2.0f)))
+  }
+  it should "properly compute addition and subtraction" in {
+    assert(SimpleParser.parse("1.0+2.0-3.0") === BopExpr(ValueExpr(NumVal(1.0f)), PlusBop, BopExpr(ValueExpr(NumVal(2.0f)), MinusBop, ValueExpr(NumVal(3.0f)))))
+  }
 }
