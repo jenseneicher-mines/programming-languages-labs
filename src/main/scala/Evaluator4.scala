19c0be56a063224c5951c5b97948848a553cfc3c
Zachary Smeton <zsmeton@yahoo.com>
2020-04-15 13:13:43 -0600

    Added support for strings and numbers with +,<,<=,>,>=

--- src/main/scala/Evaluator4.scala
+++ src/main/scala/Evaluator4.scala
@@ -16,7 +16,6 @@ object Evaluator4 {
       case BopExpr(e1,TimesBop,e2) => NumVal(toNum(eval(env, e1)) * toNum(eval(env, e2)))
       case BopExpr(e1,DivBop,e2) => NumVal(toNum(eval(env, e1)) / toNum(eval(env, e2)))
 
-
       case BopExpr(e1,EqBop,e2) => BoolVal(eval(env, e1) == eval(env, e2))
       case BopExpr(e1,NeqBop,e2) => BoolVal(eval(env, e1) != eval(env, e2))
       case UopExpr(NegUop,e) => NumVal(-toNum(eval(env, e)))
@@ -25,6 +24,7 @@ object Evaluator4 {
         val (_,v) = readEnvironment(env, x)
         v
       }
+
       case IfExpr(c, e1, e2) => if(toBool(eval(env, c))) eval(env, e1) else eval(env, e2)
       case LetExpr(Immutable, v, e1, e2) => eval(pushEnvironment(env, v, (Immutable,eval(env, e1))), e2)
       case PrintExpr(e) => {
@@ -34,10 +34,55 @@ object Evaluator4 {
 
       // TODO:
       // - PlusBop
+      case BopExpr(e1,PlusBop,e2) => {
+        (eval(env, e1), eval(env, e2)) match {
+          case (NumVal(e1n), NumVal(e2n)) => NumVal(e1n + e2n)
+          case (StringVal(e1s), e2_) => StringVal(e1s + toStr(e2_))
+          case (e1_, StringVal(e2s)) => StringVal(toStr(e1_) + e2s)
+          case (_,_) => throw UnimplementedError(e)
+        }
+      }
       // - LteBop
+      case BopExpr(e1,LteBop,e2) => {
+        (eval(env, e1), eval(env, e2)) match {
+          case (NumVal(e1n), NumVal(e2n)) => BoolVal(e1n <= e2n)
+          case (NumVal(e1n), e2_) => BoolVal(e1n <= toNum(e2_))
+          case (e1_, NumVal(e2n)) => BoolVal(toNum(e1_) <= e2n)
+          case (StringVal(e1s), StringVal(e2s)) => BoolVal(e1s.compareTo(e2s) <= 0)
+          case (_,_) => throw UnimplementedError(e)
+        }
+      }
       // - LtBop
+      case BopExpr(e1,LtBop,e2) => {
+        (eval(env, e1), eval(env, e2)) match {
+          case (NumVal(e1n), NumVal(e2n)) => BoolVal(e1n < e2n)
+          case (NumVal(e1n), e2_) => BoolVal(e1n < toNum(e2_))
+          case (e1_, NumVal(e2n)) => BoolVal(toNum(e1_) < e2n)
+          case (StringVal(e1s), StringVal(e2s)) => BoolVal(e1s.compareTo(e2s) < 0)
+          case (_,_) => throw UnimplementedError(e)
+        }
+      }
       // - GteBop
+      case BopExpr(e1,GteBop,e2) => {
+        (eval(env, e1), eval(env, e2)) match {
+          case (NumVal(e1n), NumVal(e2n)) => BoolVal(e1n >= e2n)
+          case (NumVal(e1n), e2_) => BoolVal(e1n >= toNum(e2_))
+          case (e1_, NumVal(e2n)) => BoolVal(toNum(e1_) >= e2n)
+          case (StringVal(e1s), StringVal(e2s)) => BoolVal(e1s.compareTo(e2s) >= 0)
+          case (_,_) => throw UnimplementedError(e)
+        }
+      }
       // - GtBop
+      case BopExpr(e1,GtBop,e2) => {
+        (eval(env, e1), eval(env, e2)) match {
+          case (NumVal(e1n), NumVal(e2n)) => BoolVal(e1n > e2n)
+          case (NumVal(e1n), e2_) => BoolVal(e1n > toNum(e2_))
+          case (e1_, NumVal(e2n)) => BoolVal(toNum(e1_) > e2n)
+          case (StringVal(e1s), StringVal(e2s)) => BoolVal(e1s.compareTo(e2s) > 0)
+          case (_,_) => throw UnimplementedError(e)
+        }
+      }
+
       // - LambdaExpr
       // - CallExpr
 
@@ -52,6 +97,7 @@ object Evaluator4 {
         // TODO
         throw UnimplementedError(e)
       }
+
       case ValueExpr(ClosureVal(_,_)) => throw UnimplementedError(e)
       case ValueExpr(ReferenceVal(_)) => throw UnimplementedError(e)
       case LetExpr(Mutable, v, e1, e2) => throw UnimplementedError(e)
--- src/test/scala/Evaluator4Test.scala
+++ src/test/scala/Evaluator4Test.scala
@@ -17,4 +17,73 @@ class Evaluator4Test extends FlatSpec {
     val e1 = CallExpr(LambdaExpr(Some("dec"),List(("x",None)),IfExpr(BopExpr(VarExpr("x"),GtBop,ve(0f)),BopExpr(VarExpr("x"),PlusBop,CallExpr(VarExpr("dec"),List(BopExpr(VarExpr("x"),MinusBop,ve(1f))))),ve(0f)),None),List(ve(n.toFloat)))
     assert(Evaluator4.eval((List(),Map()), e1) === v((0 to n).foldLeft(0f)(_.toFloat+_.toFloat)))
   }
+
+  it should "handle + with mixed strings and integers" in {
+    // perform 1 + 1 --> 2
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),PlusBop,ve(1f))) === v(2f))
+    // perform 'hello' + (' ' + 'world') --> 'hello world'
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("hello"),PlusBop,BopExpr(ve(" "),PlusBop,ve("world")))) === v("hello world"))
+    // perform (1 + 1) + 'hello' --> '2.0hello'
+    assert(Evaluator4.eval((List(),Map()), BopExpr(BopExpr(ve(1f), PlusBop, ve(1f)), PlusBop, ve("hello"))) === v("2.0hello"))
+    // perform 'hello' + (1 + 'world') --> 'hello1.0world'
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("hello"),PlusBop,BopExpr(ve(1f), PlusBop,ve("world")))) === v("hello1.0world"))
+  }
+
+  it should "handle < and <= with mixed strings and integers" in {
+    // perform 1 < 2 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),LtBop,ve(2f))) === v(true))
+    // perform 1 < 0 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),LtBop,ve(0f))) === v(false))
+    // perform 1 <= 1 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),LteBop,ve(1f))) === v(true))
+    // perform 1 <= 0 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),LteBop,ve(0f))) === v(false))
+    
+    // perform 'apple' < 'banana' --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("apple"),LtBop,ve("banana"))) === v(true))
+    // perform 'z' < 'a' --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("z"),LtBop,ve("a"))) === v(false))
+    // perform 'a' <= 'a' --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("a"),LteBop,ve("a"))) === v(true))
+    // perform 'z' <= 'a' --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("z"),LteBop,ve("a"))) === v(false))
+
+    // perform '1' < 10 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("1"),LtBop,ve(10f))) === v(true))
+    // perform '13' < 10 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("13"),LtBop,ve(10f))) === v(false))
+    // perform '13' <= 13 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("13"),LteBop,ve(13f))) === v(true))
+    // perform 'apple' <= 13 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("apple"),LteBop,ve(13f))) === v(false))
+  }
+
+  it should "handle > and >= with mixed strings and integers" in {
+    // perform 1 > 2 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),GtBop,ve(2f))) === v(false))
+    // perform 1 > 0 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),GtBop,ve(0f))) === v(true))
+    // perform 1 >= 1 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),GteBop,ve(1f))) === v(true))
+    // perform 1 >= 3 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve(1f),GteBop,ve(3f))) === v(false))
+    
+    // perform 'apple' > 'banana' --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("apple"),GtBop,ve("banana"))) === v(false))
+    // perform 'z' > 'a' --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("z"),GtBop,ve("a"))) === v(true))
+    // perform 'a' >= 'a' --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("a"),GteBop,ve("a"))) === v(true))
+    // perform 'a' >= 'z' --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("a"),GteBop,ve("z"))) === v(false))
+
+    // perform '1' > 10 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("1"),GtBop,ve(10f))) === v(false))
+    // perform '13' > 10 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("13"),GtBop,ve(10f))) === v(true))
+    // perform '13' >= 13 --> true
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("13"),GteBop,ve(13f))) === v(true))
+    // perform 'apple' >= 13 --> false
+    assert(Evaluator4.eval((List(),Map()), BopExpr(ve("apple"),GteBop,ve(13f))) === v(false))
+  }
 }
